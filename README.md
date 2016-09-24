d849f5bc90e8bce6b97ca6b69639dee6e8f6ba79
amalalex <amalalex.t@gmail.com>
2016-09-24 18:49:44 -0400

    project setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # angular2-quickstart
 Setup Angular Project with required Files and directories
+
+Steps to setup custom dist or output directory
+1. Change dist directory mapping in systemjs.config.js
+    app: 'app'    --->    app: 'dist'
+
+2. Add outDir in the tsconfig.json
+
+    "outDir" : "dist"
+
+3. run npm start
--- systemjs.config.js
+++ systemjs.config.js
@@ -11,7 +11,7 @@
     // map tells the System loader where to look for things
     map: {
       // our app is within the app folder
-      app: 'app',
+      app: 'resources/js',
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
       '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -35,7 +35,7 @@
         defaultExtension: 'js'
       },
       'angular2-in-memory-web-api': {
-        main: './index.js',
+        main: '../resources/js/index.js',
         defaultExtension: 'js'
       }
     }
